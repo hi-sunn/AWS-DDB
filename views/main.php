fa368028ad7e9eaa69371ca96537eaebaf7fa3d0
Ihsan Dz <ihsan.dzahri@digitalsymphony.it>
2023-05-15 12:53:22 +0800

    added new features
    - search builder
    - export to excel & csc

--- views/main.php
+++ views/main.php
@@ -110,7 +110,7 @@ foreach ($tablelist_4thBatch['TableNames'] as $value) {
     array_push($tablenameArray, $value);
 }
 
-// echo $_SERVER["DOCUMENT_ROOT"]."/AwsDDB/";
+
 ?>
 
 <!DOCTYPE html>
@@ -121,43 +121,58 @@ foreach ($tablelist_4thBatch['TableNames'] as $value) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Default UI src -->
-    <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script> -->
-    <!-- <link rel="stylesheet" href="./node_modules//datatables.net-dt/css/jquery.dataTables.min.css" /> -->
-    <!-- <script src="./node_modules/datatables.net/js/jquery.dataTables.min.js"></script> -->
+    <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./node_modules//datatables.net-dt/css/jquery.dataTables.min.css" />
+    <script src="./node_modules/datatables.net/js/jquery.dataTables.min.js"></script> -->
     <!-- Jquery UI src -->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js" integrity="sha256-VSu9DD6vJurraXgjxQJv9BuzedGfJm7XEgPQQehKBlw=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.jqueryui.min.js" integrity="sha256-39KLqmdnivnZ0OGcx+5CoAcwdauI0bdEDcFhdVTNit0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css" integrity="sha256-yMIVeRjJ/tC7ncxWyWtS3Hr3CwXKAijkZ+r5F3d1Gtc=" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.jqueryui.min.css" integrity="sha256-1mZHAtpOaAWOWfEqCa7ezIHD2LA1YsUM28154dMqXWA=" crossorigin="anonymous">
-
-
+    <!-- Export dependency -->
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js" integrity="sha256-dJiW4V/uPOIBxZUw2TwTxw1eSCqwzUDZIo2jDFyKBLw=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" integrity="sha256-RbP/rbx4XeYJH6eYUniR63Jk5NEV48Gjestg49cNSWY=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" integrity="sha256-Xon5hF/CqTXIN9zXCJpZrwnN6P/b8YZt//YhFS/HRpA=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" integrity="sha256-UsYCHdwExTu9cZB+QgcOkNzUCTweXr5cNfRlAAtIlPY=" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js" integrity="sha256-Ovv7z/mozqT8l4fJSUUSCC8n3e7iAXlWXHj8FLuoG58=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" integrity="sha256-9KeRjUewuF4eDFbdumgTsAXcQ154a85x0wgZFFwgS9g=" crossorigin="anonymous">
+    <!-- Search builder dependency -->
+    <script src="https://cdn.datatables.net/searchbuilder/1.4.2/js/dataTables.searchBuilder.min.js" integrity="sha256-JRRv5UZEd/vpVB1Q0EAEnASffb5O2oWdcNWXz9g5NNg=" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/datetime/1.4.1/js/dataTables.dateTime.min.js" integrity="sha256-+K//LRFhDW/cq6jmm/qL9NqbK04P7oeGrWJZDB/MtKE=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/searchbuilder/1.4.2/css/searchBuilder.dataTables.min.css" integrity="sha256-Tnx4Ws7wTGvtj+BujEcplfmtO1VxagBRvB2qSp/Xyxo=" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.4.1/css/dataTables.dateTime.min.css" integrity="sha256-6YeYhBx/LkYlekMIRM1+fXzFaRCQOLKms/lrFcO6tfI=" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/css/styles.css">
-
-
     <script>
         var data = <?= $json_service ?>;
         var tableKey = <?= $json_tablekey ?>;
         var tablelist = <?= $tablelistJson ?>;
-        console.log(data);
-        console.log(tableKey);
+        // console.log(data);
+        // console.log(tableKey);
 
         $(document).ready(function() {
-            $("#myTable").DataTable({
+            let table = $("#myTable").DataTable({
                 data: data,
                 paging: true,
-                scrollY: 500,
+                scrollY: 550,
                 columns: tableKey,
-                stripe: false,
+                dom: 'Bfrtp',
+                buttons: [
+                    'excelHtml5',
+                    'csvHtml5'
+                ],
+
             });
+            new $.fn.dataTable.SearchBuilder(table, {});
+            table.searchBuilder.container().prependTo(table.table().container());
         });
+
         $(document).ready(function() {
             $("#tableName").DataTable({
                 data: [],
                 paging: false,
                 scrollY: 500,
-                columns: [1],
-                searching: true
+                columns: [1]
             });
         });
     </script>
@@ -171,24 +186,22 @@ foreach ($tablelist_4thBatch['TableNames'] as $value) {
 
         <div style="padding: 1px; border-radius:4px;" class="scroll-overflow-y tablelist-container">
             <table class="grayborder ">
-                <tr style="background-color:#FAFAFA; height:50px;">
-                    <td><b>Tables</b> <span>(<?= $table_count ?>)</span></td>
-                </tr>
                 <tbody>
-                    <tr>
-                        <td></td>
+                    <tr style="background-color:#FAFAFA; height:50px;">
+                        <td><b>Tables</b> <span>(<?= $table_count ?>)</span></td>
                     </tr>
+
+                    <?php
+                    foreach ($tablenameArray as $table) :
+
+                    ?>
+                        <tr>
+                            <td id="tablelist"><a class="table-anchor" href="?db=<?= $table ?>"><?= $table ?></a></td>
+                        </tr>
+                    <?php endforeach ?>
+
+
                 </tbody>
-
-                <?php
-                foreach ($tablenameArray as $table) :
-
-                ?>
-                    <tr>
-                        <td id="tablelist"><a class="table-anchor" href="?db=<?= $table ?>"><?= $table ?></a></td>
-                    </tr>
-                <?php endforeach ?>
-
 
             </table>
         </div>
@@ -206,8 +219,10 @@ foreach ($tablelist_4thBatch['TableNames'] as $value) {
                 <tr>
                     <td></td>
                 </tr>
+
             </tbody>
         </table>
+
     </div>
 </body>
 
